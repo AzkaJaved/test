0d9153880db04742449f7bbdc6855a587996f84a
AzkaJaved <azkaishere@gmail.com>
2022-11-21 15:30:22 +0500

    practise javascript

---
+++ abc.js
@@ -0,0 +1,14 @@
+alert("hello"); 
+//math.random gives any random number between 0 and 1 it may be a decimal  
+console.log(Math.random());   
+//if you want any random number between 0 and 100 then 
+console.log(Math.random()*100);                    
+//if you want a whole number use math.floor it rounds to lower number 
+console.log(Math.floor(Math.random()*100));
+//in-line comments
+
+/*
+multi-line
+comments
+*/
+
